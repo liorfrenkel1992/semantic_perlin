6b0eab9f64c5af94f9a914734987f27ad9b7ca3a
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-08-03 18:48:00 +0300

    Update transform.py

--- transform.py
+++ transform.py
@@ -3,16 +3,16 @@ import numpy as np
 from perlin2d import generate_perlin_noise_2d, generate_fractal_noise_2d
 
 
-def generate_simulated(image, alpha):
+def generate_simulated(image, res, alpha=0.5, octaves=1):
 # This method gets a PIL image, alpha (0-1) and perlin noise as input 
 #and returns the combined weighted noisy PIL image
 	width, height = image.size
-	if width%8 != 0 or height%8 != 0:
+	if width%(res*octaves) != 0 or height%(res*octaves) != 0:
 		new_image = image.resize((width-(width%8), height-(height%8)))
 	
 	new_width, new_height = new_image.size
 	print('resized image shape: ', new_image.size)
-	np_perlin = generate_perlin_noise_2d((new_width,new_height), (8,8))
+	np_perlin = generate_fractal_noise_2d((new_width,new_height), (8,8), octaves)
 	np_image = np.asarray(new_image)
 	np_perlin = np.repeat(np_perlin[:, :, np.newaxis], 3, axis=2)
 	np_perlin = np.transpose(np_perlin, (1, 0, 2))
@@ -28,7 +28,9 @@ if __name__ == '__main__':
 	
 	im = Image.open('/data/image_processing/data/ADEChallengeData2016/images/training/ADE_train_00000001.jpg')
 	alpha = 0.5
-	combined_image = generate_simulated(im, alpha)
+	octaves = 2
+	res = (8,8)
+	combined_image = generate_simulated(im, res, alpha, octaves)
 
 	np.random.seed(0)
 	plt.imshow(combined_image)
