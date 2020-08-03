9eaacba1890e6998c0afbc1ffcedea69e9e13e1d
liorfrenkel1992 <64976963+liorfrenkel1992@users.noreply.github.com>
2020-08-03 20:41:27 +0300

    Update transform.py

--- transform.py
+++ transform.py
@@ -19,15 +19,16 @@ def generate_simulated(image, res, alpha=0.5, octaves=1):
 	new_width, new_height = new_image.size
 	np_perlin = generate_fractal_noise_2d((new_width,new_height), (8,8), octaves)
 	np_image = np.asarray(new_image).astype('uint8')
-	#np_perlin = np.repeat(np_perlin[:, :, np.newaxis], 3, axis=2)
-	np_perlin = np.transpose(np_perlin, (1, 0))
-	# Convert perlin images to values between 0 and 255
+	np_perlin = np.repeat(np_perlin[:, :, np.newaxis], 3, axis=2)
+	np_perlin = np.transpose(np_perlin, (1, 0, 2))
+	# Convert perlin image pixels to values between 0 and 255
 	np_perlin = ((np_perlin + 1) * (1/2 * 255)).astype('uint8')
 	print('perlin shape: ', np_perlin.shape)
 	print('resized image shape: ', np_image.shape)
-	np_combined = np.zeros(np_image.shape)
-	for layer in range(np_image.shape[2]):
-		np_combined[:,:,layer] = alpha*np_image[:,:,layer] + (1-alpha)*np_perlin
+	#np_combined = np.zeros(np_image.shape)
+	#for layer in range(np_image.shape[2]):
+		#np_combined[:,:,layer] = alpha*np_image[:,:,layer] + (1-alpha)*np_perlin
+	np_combined = alpha*np_image + (1-alpha)*np_perlin
 	print(np_perlin)
 	print(np_image)
 	combined = Image.fromarray(np_combined.astype('uint8'))
